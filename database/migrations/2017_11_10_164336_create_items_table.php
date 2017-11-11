549c82bd5e0afdb5bfb2fbd4372d1227b2b573e0
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-11 10:45:28 -0500

    new migrations

---
+++ database/migrations/2017_11_10_164336_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->unsignedInteger('wish_list_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('wish_list_id')
+                ->references('id')
+                ->on('wish_lists')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
