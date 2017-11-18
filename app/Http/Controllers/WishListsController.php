60eaea7d08d137aa1a022326ce2270455187daa5
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-18 16:54:49 -0500

    fill out controller

--- app/Http/Controllers/WishListsController.php
+++ app/Http/Controllers/WishListsController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\WishList;
+
 class WishListsController extends Controller
 {
     public function index()
     {
+        return view('wish-lists.index');
+    }
 
+    public function show(WishList $list)
+    {
+        return view('wish-lists.show')->with('wishList', $list);
     }
 }
