1d70cf5215f354a14d89e855627bfd8d9be3c65d
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-22 12:18:11 -0500

    truncate on seed

--- database/seeds/WishListTableSeeder.php
+++ database/seeds/WishListTableSeeder.php
@@ -11,6 +11,11 @@ class WishListTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('items')->truncate();
+        DB::table('wish_lists')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         factory(App\Models\WishList::class, 3)->create()->each(function ($l) {
             $items = factory(App\Models\Item::class, 5)->make();
 
