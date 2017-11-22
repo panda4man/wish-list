9a7c37251ab12f4eefbb15403dc9a3e692df549f
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-22 10:39:14 -0500

    basic markup to show lists

--- resources/views/wish-lists/index.blade.php
+++ resources/views/wish-lists/index.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="columns is-multiline">
-        <div class="column">
-
+    @if($wishLists->count() < 1)
+        <div class="columns is-centered">
+            <div class="column is-half-tablet is-third-desktop">
+                <div class="notification is-primary">
+                    You haven't created any lists yet.
+                </div>
+            </div>
         </div>
-    </div>
+    @else
+        <div class="columns is-multiline">
+            @foreach($wishLists as $list)
+                <div class="column is-half-tablet is-one-third-desktop">
+                    <div class="card">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                                {{$list->name}}
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endif
 @endsection
