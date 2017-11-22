448a35eea80190593564d6d451762f5ff0b98cea
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-22 12:30:40 -0500

    pass in wish list prop to component

--- resources/views/wish-lists/show.blade.php
+++ resources/views/wish-lists/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.base')
 
 @section('content')
+    <wish-list-details :wish-list="{{json_encode($json['wishList'])}}" inline-template>
+        <div>
 
+        </div>
+    </wish-list-details>
 @endsection
