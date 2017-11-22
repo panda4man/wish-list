3729fe43d0c0eae7448849a5a02e4ca23c239865
Andrew Clinton <ddrew.clinton@gmail.com>
2017-11-22 12:29:47 -0500

    fill out the wish list transformer

--- app/Transformers/WishListTransformer.php
+++ app/Transformers/WishListTransformer.php
@@ -2,19 +2,35 @@
 
 namespace App\Transformers;
 
+use App\Models\WishList;
 use League\Fractal\TransformerAbstract;
 
 class WishListTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = ['items'];
+
     /**
      * A Fractal transformer.
      *
+     * @param WishList $list
      * @return array
      */
-    public function transform()
+    public function transform(WishList $list)
     {
         return [
-            //
+            'id'   => $list->id,
+            'name' => $list->name,
         ];
+    }
+
+    public function includeItems(WishList $list)
+    {
+        if(!$list->relationLoaded('items')) {
+            $list->load('items');
+        }
+
+        $items = $list->items;
+
+        return $this->collection($items, new ItemTransformer());
     }
 }
